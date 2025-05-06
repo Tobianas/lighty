81ccff96adc3a4400a9981a213ffd28bbea4a4d9
tobias.pobocik <tobias.pobocik@pantheon.tech>
2025-05-06 13:13:51 +0200

    WIP
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- lighty-examples/lighty-community-aaa-restconf-app/src/main/java/io/lighty/kit/examples/community/aaa/restconf/Main.java
+++ lighty-examples/lighty-community-aaa-restconf-app/src/main/java/io/lighty/kit/examples/community/aaa/restconf/Main.java
@@ -110,16 +110,6 @@ public final class Main {
         // 2. Initialize and start Restconf server
         final LightyJettyServerProvider jettyServerBuilder = new LightyJettyServerProvider(new InetSocketAddress(
                 restconfConfiguration.getInetAddress(), restconfConfiguration.getHttpPort()));
-        this.restconf = CommunityRestConfBuilder
-                .from(RestConfConfigUtils.getRestConfConfiguration(restconfConfiguration,
-                    this.lightyController.getServices()))
-                .withLightyServer(jettyServerBuilder)
-                .build();
-        final boolean restconfStartOk = this.restconf.start()
-                .get(modulesConfig.getModuleTimeoutSeconds(), TimeUnit.SECONDS);
-        if (!restconfStartOk) {
-            throw new ModuleStartupException("Community Restconf startup failed!");
-        }
 
         // 3. Initialize and start Lighty AAA
         final DataBroker bindingDataBroker = this.lightyController.getServices().getBindingDataBroker();
@@ -134,6 +124,20 @@ public final class Main {
             throw new ModuleStartupException("AAA module startup failed!");
         }
 
+        this.restconf = CommunityRestConfBuilder
+            .from(RestConfConfigUtils.getRestConfConfiguration(restconfConfiguration,
+                this.lightyController.getServices()))
+            .withLightyServer(jettyServerBuilder)
+            .withWebSecurer(aaaLighty.getWebContextSecurer())
+            .build();
+        final boolean restconfStartOk = this.restconf.start()
+            .get(modulesConfig.getModuleTimeoutSeconds(), TimeUnit.SECONDS);
+        if (!restconfStartOk) {
+            throw new ModuleStartupException("Community Restconf startup failed!");
+        }
+
+        // 4. Start Lighty jetty server
+        this.restconf.startServer();
     }
 
     private void closeLightyModule(final LightyModule module) {
